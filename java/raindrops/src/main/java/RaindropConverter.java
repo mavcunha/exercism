596652e687829780caa7e50ff3bea95239d108cc
Marco Valtas <mavcunha@gmail.com>
2017-04-24 22:36:56 -0300

    Second version

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,22 +1,18 @@
-import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RaindropConverter {
     public String convert(int number) {
-        List<Integer> collect = IntStream.range(1, number+1)
+        String collect = IntStream.range(1, number+1)
                 .filter(i -> number % i == 0)
                 .boxed()
-                .collect(Collectors.toList());
-        String collect1 = collect.stream()
-                .map(new Function<Integer, String>() {
-                    @Override
-                    public String apply(Integer integer) {
-                        return integer == 3 ? "Pling" : integer == 5 ? "Plang" : integer == 7 ? "Plong" : "";
-                    }
-                }).collect(Collectors.joining());
+                .map(this::soundOf)
+                .collect(Collectors.joining());
 
-        return collect1.isEmpty() ? String.valueOf(number) : collect1;
+        return collect.isEmpty() ? String.valueOf(number) : collect;
+    }
+
+    private String soundOf(Integer integer) {
+        return integer == 3 ? "Pling" : integer == 5 ? "Plang" : integer == 7 ? "Plong" : "";
     }
 }
