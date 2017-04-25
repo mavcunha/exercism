f62c9c0bbfc91ae19028982c8f79f2b2467e6e10
Marco Valtas <mavcunha@gmail.com>
2017-04-24 22:54:08 -0300

    Third version

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,18 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.collectingAndThen;
+
 public class RaindropConverter {
+
+    private final Map<Integer,String> sounds = new HashMap<Integer, String>(){{
+        put(3, "Pling");
+        put(5, "Plang");
+        put(7, "Plong");
+    }};
+
     public String convert(int number) {
-        String collect = IntStream.range(1, number+1)
-                .filter(i -> number % i == 0)
-                .boxed()
-                .map(this::soundOf)
-                .collect(Collectors.joining());
-
-        return collect.isEmpty() ? String.valueOf(number) : collect;
+        return IntStream.rangeClosed(1, number)
+                .filter(i -> number % i == 0).boxed()
+                .map(i -> sounds.getOrDefault(i, ""))
+                .collect(collectingAndThen(
+                        Collectors.joining(),
+                        s -> s.isEmpty() ? String.valueOf(number) : s));
     }
 
-    private String soundOf(Integer integer) {
-        return integer == 3 ? "Pling" : integer == 5 ? "Plang" : integer == 7 ? "Plong" : "";
-    }
 }
