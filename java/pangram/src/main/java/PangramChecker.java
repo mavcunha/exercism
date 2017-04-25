ae7589b33cbf02b2e613d62391ac0b85509977ec
Marco Valtas <mavcunha@gmail.com>
2017-04-24 22:00:12 -0300

    Using a Set to aggregate all input

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,33 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PangramChecker {
 
-    private List<Character> atLeastOnceChars;
-
-    public PangramChecker() {
-        atLeastOnceChars = "abcdefghijklmnopqrstuvxywz".chars()
-                .mapToObj(c -> ((char) c))
-                .collect(Collectors.toList());
-    }
+    private List<String> atLeastOnceChars =
+            Arrays.stream("abcdefghijklmnopqrstuvxywz".split("")).collect(Collectors.toList());
 
     public boolean isPangram(String input) {
-        final char[] inputChars = input.toLowerCase(Locale.ENGLISH).toCharArray();
+        return atLeastOnceChars.stream()
+                .allMatch(Arrays.stream(input.toLowerCase().split(""))
+                        .collect(Collectors.toSet())::contains);
 
-        return remainingMandatoryChars(inputChars) == 0;
-    }
-
-    private int remainingMandatoryChars(char[] inputChars) {
-        List<Character> atLeastOnce = newListCopy(this.atLeastOnceChars);
-        for (int i = 0; i < inputChars.length && atLeastOnce.size() != 0; i++) {
-            atLeastOnce.remove(Character.valueOf(inputChars[i]));
-        }
-        return atLeastOnce.size();
-    }
-
-    private List<Character> newListCopy(List<Character> orig) {
-        return new ArrayList<>(orig);
     }
 }
