bf16000f9410e1aeae74365afe0f180295a8f876
Marco Valtas <mavcunha@gmail.com>
2017-04-24 23:50:05 -0300

    Extract method for square

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -7,15 +7,19 @@ public final class DifferenceOfSquaresCalculator {
 
     public int computeSquareOfSumTo(int number) {
         return until(number).boxed()
-                .collect(collectingAndThen(summingInt(a -> a), i -> i * i));
+                .collect(collectingAndThen(summingInt(a -> a), this::square));
     }
 
     public int computeSumOfSquaresTo(int number) {
-        return until(number).map(a -> a * a).sum();
+        return until(number).map(this::square).sum();
     }
 
     public int computeDifferenceOfSquares(int number) {
         return computeSquareOfSumTo(number) - computeSumOfSquaresTo(number);
+    }
+
+    private int square(Integer i) {
+        return i * i;
     }
 
     private IntStream until(int number) {
