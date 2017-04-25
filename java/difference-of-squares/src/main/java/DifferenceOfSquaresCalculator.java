8e010dd3f61274b65372be08a857c52b3360dbff
Marco Valtas <mavcunha@gmail.com>
2017-04-24 23:37:00 -0300

    Extract range

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,18 +1,24 @@
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.summingInt;
 
 public final class DifferenceOfSquaresCalculator {
 
     public int computeSquareOfSumTo(int number) {
-        return IntStream.rangeClosed(1, number).boxed().collect(
-                Collectors.collectingAndThen(Collectors.summingInt(a -> a),i -> i * i));
+        return until(number).boxed()
+                .collect(collectingAndThen(summingInt(a -> a), i -> i * i));
     }
 
     public int computeSumOfSquaresTo(int number) {
-        return IntStream.rangeClosed(1, number).map(a -> a * a).sum();
+        return until(number).map(a -> a * a).sum();
     }
 
     public int computeDifferenceOfSquares(int number) {
         return computeSquareOfSumTo(number) - computeSumOfSquaresTo(number);
+    }
+
+    private IntStream until(int number) {
+        return IntStream.rangeClosed(1, number);
     }
 }
