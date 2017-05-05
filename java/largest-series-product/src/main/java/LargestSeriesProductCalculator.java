445f9d71979077a62deb659aba973123ff941651
Marco Valtas <mavcunha@gmail.com>
2017-05-05 18:09:23 -0300

    Extract messages to constants

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -4,11 +4,16 @@ import static java.util.stream.IntStream.rangeClosed;
 
 public class LargestSeriesProductCalculator {
 
+    private static final String NON_NULL = "String to search must be non-null.";
+    private static final String ONLY_DIGITS = "String to search may only contains digits.";
+    private static final String SERIES_LESS_EQ_SEARCH = "Series length must be less than or equal to the length of the string to search.";
+    private static final String SERIES_NON_NEGATIVE = "Series length must be non-negative.";
+
     private final long[] digits;
 
     public LargestSeriesProductCalculator(String digits) {
-        ensure(digits != null, "String to search must be non-null.");
-        ensure(digits.matches("^\\d*$"), "String to search may only contains digits.");
+        ensure(digits != null, NON_NULL);
+        ensure(digits.matches("^\\d*$"), ONLY_DIGITS);
 
         this.digits = digits.chars().boxed()
                 .mapToLong(Character::getNumericValue)
@@ -16,8 +21,8 @@ public class LargestSeriesProductCalculator {
     }
 
     public long calculateLargestProductForSeriesLength(int w) {
-        ensure(w <= digits.length, "Series length must be less than or equal to the length of the string to search.");
-        ensure(!(w < 0), "Series length must be non-negative.");
+        ensure(w <= digits.length, SERIES_LESS_EQ_SEARCH);
+        ensure(!(w < 0), SERIES_NON_NEGATIVE);
 
         return rangeClosed(0, digits.length - w)
                 .mapToLong(i ->
@@ -26,8 +31,8 @@ public class LargestSeriesProductCalculator {
                 .sorted().max().orElse(0);
     }
 
-    private void ensure(boolean test, final String message) {
-        if(!test) {
+    private void ensure(boolean expected, final String message) {
+        if(!expected) {
             throw new IllegalArgumentException(message);
         }
     }
