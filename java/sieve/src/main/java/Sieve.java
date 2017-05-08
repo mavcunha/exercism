88eca5a40933f6c53db02586a618e33f79fc8863
Marco Valtas <mavcunha@gmail.com>
2017-05-08 16:15:38 -0300

    Not efficient but with streams

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -3,22 +3,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.IntStream.rangeClosed;
+
 public class Sieve {
 
-    private final List<Integer> notPrimes = new ArrayList<>();
-    private final int number;
+    private List<Integer> primes = new ArrayList<>();
 
     public Sieve(int number) {
-        this.number = number;
+        int sqrt = (int) Math.sqrt((double) number);
 
-        int sqrt = (int) Math.sqrt((double) this.number);
+        primes = upTo(number)
+                .filter(k -> upTo(sqrt)
+                        .flatMap(n -> upTo(number)
+                                .map(m -> n * m)
+                                .filter(j -> j <= number))
+                        .noneMatch(l -> k == l))
+                .boxed().collect(Collectors.toList());
+    }
 
-        IntStream.rangeClosed(2, sqrt)
-                .flatMap(n -> IntStream.rangeClosed(2, number).map(m -> n*m))
-                .forEach(notPrimes::add);
+    private IntStream upTo(int number) {
+        return rangeClosed(2, number);
     }
 
     public List<Integer> getPrimes() {
-        return IntStream.rangeClosed(2, number).boxed().filter(n -> !notPrimes.contains(n)).collect(Collectors.toList());
+        return primes;
     }
 }
