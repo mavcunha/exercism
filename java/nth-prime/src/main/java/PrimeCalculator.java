c55104879824e7d1c5612f26017be3a83a51d121
Marco Valtas <mavcunha@gmail.com>
2017-05-16 15:55:38 -0300

    Simple implementation gets the job done

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,12 +1,18 @@
+import static java.lang.Math.sqrt;
 import static java.util.stream.IntStream.iterate;
+import static java.util.stream.IntStream.rangeClosed;
 
 public class PrimeCalculator {
 
     public int nth(int i) {
         if(!(i > 0)) throw new IllegalArgumentException();
 
-        return iterate(1, n -> n + 1).filter(RosettaModifiedPrimeTest::isPrime)
+        return iterate(2, n -> n + 1).filter(this::isPrime)
                 .limit(i).reduce((f, s) -> s).getAsInt();
+    }
+
+    private boolean isPrime(int n) {
+        return rangeClosed(2, (int) sqrt(n)).noneMatch(i -> n % i == 0);
     }
 
 }
