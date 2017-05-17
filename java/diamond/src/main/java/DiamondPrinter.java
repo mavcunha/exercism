1b22b533ce57311c2ff37aa9c035417eda4bb819
Marco Valtas <mavcunha@gmail.com>
2017-05-16 23:11:28 -0300

    Solution without ifs

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -1,37 +1,42 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-import static java.lang.String.join;
-import static java.util.stream.IntStream.concat;
-import static java.util.stream.IntStream.range;
-import static java.util.stream.IntStream.rangeClosed;
+import static java.util.stream.IntStream.*;
 
 final class DiamondPrinter {
-
 
     public List<String> printToList(char c) {
         IntStream ay = range('A', c);
         IntStream za = rangeClosed('A', c).map(z -> c - z + 'A');
 
-        return concat(ay, za).mapToObj(z -> diamondLine(c, z)).collect(Collectors.toList());
+        return concat(ay, za)
+                .mapToObj(z -> diamondLine(c, (char) z))
+                .collect(Collectors.toList());
     }
 
-    private String diamondLine(char c, int z) {
-        Character ch = (char) z;
+    private String diamondLine(char last, char curr) {
+        List<String> line = emptyLine(last - 'A' + 1);
 
-        int os = c - z;
-        int is = 2 * (z - 'A') - 1;
+        int lastIdx = line.size() - 1;
 
-        if(z == 'A') {
-            return spaces(os) + ch + spaces(os);
-        } else {
-            return spaces(os) + ch + spaces(is) + ch + spaces(os);
-        }
+        line.set(lastIdx - (curr - 'A'), String.valueOf(curr));
+
+        return Stream.of(line, reverse(line.subList(0, lastIdx)))
+                .flatMap(List::stream)
+                .collect(Collectors.joining());
     }
 
-    private String spaces(int i) {
-        return join("", Collections.nCopies(i, " "));
+    private List<String> reverse(List<String> line) {
+        List<String> reversed = new ArrayList<>(line);
+        Collections.reverse(reversed);
+        return reversed;
+    }
+
+    private List<String> emptyLine(int size) {
+        return new ArrayList<>(Collections.nCopies(size, " "));
     }
 }
