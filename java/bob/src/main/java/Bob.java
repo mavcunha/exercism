b17b5012e98fa3b3b26b5df7a683e132fdf66041
Marco Valtas <mavcunha@gmail.com>
2017-05-22 21:45:02 -0400

    Playing with Functions

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,12 +1,29 @@
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public class Bob {
+
     public String hey(String msg) {
-        if (msg.trim().isEmpty()) {
-            return "Fine. Be that way!";
-        } else if (msg.equals(msg.toUpperCase()) && !msg.equals(msg.toLowerCase())) {
-            return "Whoa, chill out!";
-        } else if (msg.endsWith("?")) {
-            return "Sure.";
-        }
-        return "Whatever.";
+        return Stream.of(
+                silence("Fine. Be that way!"),
+                yell("Whoa, chill out!"),
+                question("Sure."))
+                .map(f -> f.apply(msg))
+                .filter(Objects::nonNull)
+                .findFirst().orElse("Whatever.");
+    }
+
+    private Function<String, String> yell(String response) {
+        return msg -> msg.equals(msg.toUpperCase()) && !msg.equals(msg.toLowerCase()) ?
+                response : null;
+    }
+
+    private Function<String, String> silence(String response) {
+        return msg -> msg.trim().isEmpty() ? response : null;
+    }
+
+    private Function<String, String> question(String response) {
+        return msg -> msg.endsWith("?") ? response : null;
     }
 }
